e91d7f9f40d7a92a2d224766796de9dc31426bcb
Huy Dat <dat.nguyen.d16ptit@gmail.com>
2020-10-05 15:48:32 +0700

    user login logout

--- resources/views/users/pages/home.blade.php
+++ resources/views/users/pages/home.blade.php
@@ -2,13 +2,4 @@
 
 @section('content')
     @include("users.elements.slide")
-    @include('users.components.homes.introduce_component')
-    @include('users.components.homes.new_shoes_arrival_component')
-    @include('users.components.homes.sale_component')
-    @include('users.components.homes.introduce_product_component')
-    @include('users.components.homes.satisfied_customer_component')
-@endsection
-
-@section('js')
-
 @endsection
